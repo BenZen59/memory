167f06392311c09f9e29d7c5a7254fd1d88fb9cc
BenZen59 <benoit.zenone@gmail.com>
2022-12-08 16:46:28 +0100

    add card and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import card from './Card.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <span className='memory'>Memory</span>
+      <div className='cardContent'>
+        {card.map((event) => {
+          return <img src={event} alt='card' />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import Card1 from './img/card1.png';
+import Card2 from './img/card2.png';
+import Card3 from './img/card3.png';
+import Card4 from './img/card4.png';
+import Card5 from './img/card5.png';
+import Card6 from './img/card6.png';
+import Card7 from './img/card7.png';
+import Card8 from './img/card8.png';
+import CardDos from './img/carddos.png';
+
+const card = [
+  CardDos,
+  CardDos,
+  CardDos,
+  CardDos,
+  CardDos,
+  CardDos,
+  CardDos,
+  CardDos,
+];
+
+export default card;
